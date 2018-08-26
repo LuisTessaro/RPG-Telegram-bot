11446c54579696699b5cf2f22946b1574f9c73ea
Luís Henrique Tessaro Pedroso <lh.pedrosot@hotmail.com>
2018-08-26 20:14:57 -0300

    Revert "Revert "Safety""
    This reverts commit 7bc29e27f6c6dd89322a638f5cc01a456b4a50e9.

--- app/config/configs.js
+++ app/config/configs.js
@@ -1,8 +1,10 @@
 var load = require('consign');
+var fs = require('fs');
+var token = fs.readFileSync('C:/data/token.txt', 'utf8');
 const TeleBot = require('telebot');
 
 module.exports = function () {
-    var bot = new TeleBot('token-token');
+    var bot = new TeleBot(token);
 
     load({ cwd: 'app' })
         .include('infra')

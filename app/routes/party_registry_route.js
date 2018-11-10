2a0bcd2720f2197d1b6e92b797b7fe05695c1422
Luís Henrique Tessaro Pedroso <lh.pedrosot@hotmail.com>
2018-11-10 10:44:02 -0200

    Party registry

--- app/routes/party_registry_route.js
+++ app/routes/party_registry_route.js
@@ -4,10 +4,23 @@ module.exports = function (bot) {
   bot.on(/^\/newParty (.+)$/, (msg, props) => {
     player_funcs.handlePlayerExists(msg, bot)
       .then(function (resolve) {
-        //register
+        const partyName = props.match[1]
+        var party_dao = new bot.infra.DAO.party_dao()
+        party_dao.insertParty(formParty(partyName, resolve.name))
+        return bot.sendMessage(msg.from.id, 'Party Registerd.')
       })
       .catch(function (reject) {
         return bot.sendMessage(msg.from.id, 'use /register to set up an account')
       })
   })
+}
+
+function formParty(name, leader) {
+  return {
+    name: name,
+    leader: leader,
+    players: [{
+      name: leader
+    }],
+  }
 }

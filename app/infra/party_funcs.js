128bf7b7dabe4a353b7582754a1986a573efff1c
Luís Henrique Tessaro Pedroso <lh.pedrosot@hotmail.com>
2018-11-10 10:50:41 -0200

    Party functions mockup

---
+++ app/infra/party_funcs.js
@@ -0,0 +1,14 @@
+function party_funcs () { }
+
+party_funcs.prototype.addPlayerToParty = function (msg, player, bot) {
+}
+
+party_funcs.prototype.removePlayersFromParty = function (msg, player, bot) {
+}
+
+party_funcs.prototype.handlePartyExists = function (msg, bot) {
+}
+
+module.exports = function () {
+  return party_funcs
+}

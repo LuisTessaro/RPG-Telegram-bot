74b4e7bb38d891216d42b4b79d167f859bdeec47
unknown <lh.pedrosot@hotmail.com>
2019-06-25 22:31:54 -0300

    uri name change

--- model/mongoose.js
+++ model/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.SERVER_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }, (err) => {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }, (err) => {
     if (err) return console.log(err)
     console.log('Mongoose Connected!')
 })
